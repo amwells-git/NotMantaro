09f162a1643500c04907c158ad656e2a4efb4060
amwells-git <amwells2021@gmail.com>
2024-03-04 18:21:37 -0600

    Remove Comment Block from index.js

--- src/index.js
+++ src/index.js
@@ -15,38 +15,6 @@ const client = new Client({
     ]
 });
 
-/*
-
-//listen to interactions
-client.on('interactionCreate', (interaction) => {
-    //don't do anything if not slash command
-    if (!interaction.isChatInputCommand()) return;
-
-    //test commands
-    if (interaction.commandName === 'testo') {
-        const num1 = interaction.options.get('number')?.value;
-        const num2 = interaction.options.get('numbertwo')?.value;
-        interaction.reply(`why'd you give me ${num1} & ${num2}?`);
-    }
-
-    //check for daily command
-    if (interaction.commandName === 'daily'){
-        interaction.reply("this isn't set up yet dumbass");
-    }
-});
-
-//react to messages
-client.on('messageCreate', (message) => {
-    //check channel is test channel id
-    if (message.channelId === '1212955162377715742' && !message.author.bot) {
-        if (message.content === 'daily'){
-            message.reply("That's not how this works");
-        }
-    }
-});
-
- */
-
 //event handler
 eventHandler(client);
 
