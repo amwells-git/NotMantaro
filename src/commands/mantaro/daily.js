9d11de2908d84a1f667fdc2afd33f3d66fed3e59
amwells-git <amwells2021@gmail.com>
2024-03-05 22:26:07 -0600

    Started Work on Daily

--- src/commands/mantaro/daily.js
+++ src/commands/mantaro/daily.js
@@ -1,5 +1,9 @@
 //uncomment if you need options
 //const { ApplicationCommandOptionType } = require('discord.js');
+//import schema
+const User = require('../../models/userProfile');
+//import calculation function
+const calculateDailyCurrency = require('../../utils/calculateDailyCurrency');
 
 module.exports = {
     name: 'daily',
@@ -9,7 +13,46 @@ module.exports = {
     // options: Object[],
     // deleted: Boolean,
 
-    callback: (client, interaction) => {
-        interaction.reply(`This STILL isn't implemented`);
+    callback: async (client, interaction) => {
+        //check to make sure command is run inside server
+        if (!interaction.inGuild()) {
+            interaction.reply({
+                content: "This command can only be executed inside a server.",
+                ephemeral: true,
+            });
+            return;
+        }
+
+        try {
+            await interaction.deferReply();
+
+            //create query
+            let user = await User.findOne({
+                userId: interaction.member.id,
+            });
+
+            //if user found, check streak timing
+            if (user) {
+                const lastDailyDate = user.lastDailyCollected?.toDateString();
+                const currentDate = new Date().toDateString();
+
+                //compare dates
+                interaction.editReply("Not Implemented")
+                return;
+            }
+            //if user not found, create a new one!
+            else {
+                user = new User({
+                    userId: interaction.member.id,
+                })
+            }
+
+            //calculate daily currency and streak
+            const calculatedCurrency = calculateDailyCurrency(userProfile)
+            userProfile.balance += calculatedCurrency.totalCurrency;
+
+        } catch (error) {
+            console.log(`Error handling /daily: ${error}`);
+        }
     }
-}
+};
