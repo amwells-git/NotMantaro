3dd1885f590c83a475bf4c07755cc20882fbccb3
amwells-git <amwells2021@gmail.com>
2024-03-07 18:00:02 -0600

    Added testo.js Meant to allow deletion of unused /testo command

---
+++ src/commands/misc/testo.js
@@ -0,0 +1,15 @@
+//uncomment if you need options
+//const { ApplicationCommandOptionType } = require('discord.js');
+
+module.exports = {
+    name: 'testo',
+    description: 'Tests Option Slash Commands',
+    // devOnly: Boolean,
+    // testOnly: Boolean,
+    // options: Object[],
+    deleted: true,
+
+    callback: (client, interaction) => {
+
+    }
+}
