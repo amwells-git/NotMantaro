0c0fd0befadcf4bd1b9d66638cd45f932dea499c
amwells-git <amwells2021@gmail.com>
2024-03-09 16:06:37 -0600

    Added Comments to pokemon.js

--- src/utils/game/pokemon/pokemon.js
+++ src/utils/game/pokemon/pokemon.js
@@ -4,12 +4,15 @@ const {EmbedBuilder} = require("discord.js");
 const randomPokemonName = require('./randomPokemonName');
 
 module.exports = (client, interaction) => {
+    //get random pokemon for game
     let pokemon = randomPokemonName();
+    //create embed
     let pokembed = new EmbedBuilder()
         .setColor(0xFF66D6)
         .setImage(`attachment://${pokemon}.png`)
         .setFooter({text: 'You have 5 attempts and 1 minute. (Type end to end the game)'})
         .setAuthor({name: `Who's that pokemon?`, iconURL: interaction.user.displayAvatarURL()});
 
+    //send embed and start game
     interaction.editReply({embeds: [pokembed], files: [`./src/assets/pokemonImages/${pokemon}.png`]});
 }
